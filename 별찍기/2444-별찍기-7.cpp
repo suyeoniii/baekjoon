1cf9dc9f59de6343dbab2b2e4dd84380890b59d5
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2020-09-02 21:35:18 +0900

    Add files via upload

---
+++ 별찍기/2444-별찍기-7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	int num;
+	cin >> N;
+
+	for (int i = N; i > 1; i--) {
+
+		num = N - i;
+		for (int j = 0; j < i-1; j++) {
+			cout << ' ';
+		}
+		for (int j = 0; j < 2*num+1; j++) {
+			cout << '*';
+		}
+		cout << endl;
+	}
+
+	for (int i = N; i > 0; i--) {
+
+		num = N - i;
+		for (int j = 0; j < num; j++) {
+			cout << ' ';
+		}
+		for (int j = 0; j < 2 * i - 1; j++) {
+			cout << '*';
+		}
+		cout << endl;
+	}
+
+}
