9547e9b18d801aa3af1c520fc5e8a2d7941b0833
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2020-09-15 01:14:12 +0900

    2742-기찍 N

---
+++ 2742-기찍 N.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		v.push_back(make_pair(a, b));
+	}
+}
