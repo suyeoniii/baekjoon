0557547c6eef4a9c0dc854f06a3566cec8b46bb2
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2020-09-16 14:11:53 +0900

    10951-A+B-4

---
+++ 10951-A+B-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b;
+
+    while (true) {
+        cin >> a >> b;
+        if (cin.eof() == true) {
+            break;
+        }
+        cout << a + b << endl;
+    }
+}
