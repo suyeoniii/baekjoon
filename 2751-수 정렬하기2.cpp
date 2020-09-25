a3de5f7fadf48695cd0be740c887e436e8b76a36
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2020-09-25 19:39:16 +0900

    2751-수 정렬하기2

---
+++ 2751-수 정렬하기2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  int N;
+   cin>>N;
+    
+    vector<int> v;
+    
+    for(int i = 0;i<N;i++){
+  int a;
+     cin>>a;
+        v.push_back(a);
+}
+    
+    sort(v.begin(),v.end());
+    
+     for(int i = 0;i<v.size();i++){
+        cout<< v[i]<<"\n";
+     }
+}
