147b8e69798b0e82bd0ec60c428130879f0fe1bb
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2020-10-19 00:50:59 +0900

    5337-웰컴

---
+++ 5337-웰컴.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	cout << ".  .   ." << "\n";
+	cout << "|  | _ | _. _ ._ _  _" << "\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";
+}
