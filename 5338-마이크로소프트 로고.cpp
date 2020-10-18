972208181e3d6330dfd6ccf85dc06694a87c3502
suyeoniii <52654236+suyeoniii@users.noreply.github.com>
2020-10-19 00:50:41 +0900

    5338-마이크로소프트 로고

---
+++ 5338-마이크로소프트 로고.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	cout << "       _.-;;-._" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-;;-._|" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-''-._|" << "\n";
+}
