613dd3a09daed9eda3f9cba300a767245f1aacd6
ssy4230@naver.com <ssy4230@naver.com>
2020-11-29 23:52:56 +0900

    Updated At 20201129

---
+++ 15921-수찬은 마린보이야!!.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    double ans = 0;
+    for(int i = 0;i<N;i++){
+        double a;
+        cin>>a;
+    }
+    if(N==0){
+        cout<<"divide by zero";
+    }
+    else{
+        cout<<"1.00";
+    }
+
+}
