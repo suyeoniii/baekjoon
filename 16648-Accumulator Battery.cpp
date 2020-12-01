94389aa4f953dee4423fe9c5e325b44f986de9e6
ssy4230@naver.com <ssy4230@naver.com>
2020-12-02 06:47:15 +0900

    Updated At 20201202

--- 16648-Accumulator Battery.cpp
+++ 16648-Accumulator Battery.cpp
@@ -5,13 +5,15 @@ int main(){
     double t,p;
     double ans = 0;
     cin>>t>>p;
-    cout.precision(8);
+    cout.precision(10);
 
     if(p>=20){
         ans = (100-p)/t;
+        ans = (p-20)/ans+20/(ans/2);
     }
     else{
-        ans = t/(100-p+20-p)*p*2;
+        ans = (80+(20-p)*2)/t;
+        ans = p/(ans/2);
 
     }
     cout<<ans;
